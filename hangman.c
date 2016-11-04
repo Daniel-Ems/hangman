8321c4987948beff91c48c02c9544b7a6806c409
Daniel Ems <danielems3@outlook.com>
2016-11-04 00:13:57 -0400

    Work to eliminate segfault

--- hangman.c
+++ hangman.c
@@ -7,20 +7,22 @@ void hangy_hangy(char *string, char *hangman, char *current);
 int main()
 {
   //TODO: turn string into a random word from a file
-  char strings[32]; // initiate variable for testing
-  char guess[32];
+  char *strings[32]; // initiate variable for testing
+  char *guess[32];
   FILE *words;
   words = fopen("words", "r");
+  int a=0;
   while(1)
    {
-    if(fgets(guess, sizeof(guess), words) == NULL) break;
-    printf("%s", guess);
+    if(fgets(guess[a], sizeof(guess[a]), words) != NULL){
+        printf("%s", guess[a]);
+        a++;
+    }
   }
-  printf("%s\n", guess);
-  printf("%zd\n", strlen(guess));
-  strncpy(strings, guess, sizeof(strings));
-  char *string = strtok(strings, "\n ");
-  printf("%zd\n", strlen(string));
+  printf("%s\n", *guess);
+  printf("strlen(guess)%zd\n", strlen(*guess));
+  char *string = strtok(strings[1], "\n ");
+  printf("strlen(string)%zd\n", strlen(string));
 
   //TODO: make this accomodate any sized word that comes in from the file.
   char hangman[32] = {'_','_','_','_'};
@@ -31,11 +33,12 @@ int main()
   
   //TODO: put the fgets in a loop to repeatedly ask for user input 
 
-  fgets(guess, sizeof(guess), stdin);//takes in 32 bytes of user input, puts it in guess 
-  strncpy(fool, guess, sizeof(fool));//copies whats in guess and puts it in fool 
+  fgets(guess[a], sizeof(guess), stdin);//takes in 32 bytes of user input, puts it in guess 
+  strncpy(fool, guess[a], sizeof(fool));//copies whats in guess and puts it in fool 
   char *current = strtok(fool, "\n ");// creates a token from the strtok
   printf("%zd\n", strlen(current));
   hangy_hangy(string, hangman, current);
+  fclose(words);
   
 }
   //a starting loop that checks and prints input, against words, and prints out
