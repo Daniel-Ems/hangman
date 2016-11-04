e9490f9b5a5336390318e60d8697a857dfa0ad5b
Daniel Ems <danielems3@outlook.com>
2016-11-03 20:19:01 -0400

    Add a blank array for output when on good guesses

--- hangman.c
+++ hangman.c
@@ -5,7 +5,7 @@
 int main(void)
 {
   char string[5] = {'w','o','r','d'}; // initiate variable for testing
- 
+  char hangman[5] = {'_','_','_','_'};
 
   char guess[5];
   char fool[5];
@@ -14,20 +14,19 @@ int main(void)
   
   fgets(guess, 5, stdin);//takes user input, puts it in guess
    strncpy(fool, guess, 5);
-  char *current = strtok(fool, "\n");
+  char *current = strtok(fool, "\n ");
+  printf("%s\n", current);
   printf("%c -\n", fool[0]);
   
   //a basic for loop that checks the user input against or variable array 
   //poorly designed, but good for a starting point 
   for(int b=0; b<4; b++){
-    if(*current == string[b]){
-      printf("_%c__\n", string[b]);
-    }
-    else{
-      printf("sorry, %s is not in the word\n", current);
-    }
+    for(int a=0; a < 4; ++a){
+      if(current[b] == string[a]){
+        hangman[a] = current[b];
+      }
+   }
   }
-
-  
+  printf("%s\n", hangman);
 
 }
