beac3341ca65de55b6efbc77ed546cf6b49e681c
Daniel Ems <danielems3@outlook.com>
2016-11-03 20:41:04 -0400

    Place checker in function

--- hangman.c
+++ hangman.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 
+void hangy_hangy(char *string, char *hangman, char *current);
 int main(void)
 { //TODO: turn string into a random word from a file
   char string[32] = {'w','o','r','d'}; // initiate variable for testing
@@ -13,14 +14,17 @@ int main(void)
   char guess[32];
   char fool[32];
 
-  printf("%s", hangman);
+  printf("%s:", hangman);
   
   //TODO: put the fgets in a loop to repeatedly ask for user input 
   fgets(guess, 32, stdin);//takes in 32 bytes of user input, puts it in guess 
   strncpy(fool, guess, 32);//copies whats in guess and puts it in fool 
   char *current = strtok(fool, "\n ");// creates a token from the strtok
-
+  hangy_hangy(string, hangman, current);
+}
   //a starting loop that checks and prints input, against words, and prints out
+void hangy_hangy(char *string, char *hangman, char *current)
+{
   for(int b=0; b<32; b++){
     for(int a=0; a < 32; ++a){
       if(current[b] == string[a]){
@@ -29,5 +33,4 @@ int main(void)
    }
   }
   printf("%s\n", hangman);
-
 }
