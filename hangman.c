458d8a1d48cac647a525ebf5ef28814e649324ed
Daniel Ems <danielems3@outlook.com>
2016-11-03 22:17:58 -0400

    Create a file of one word and pass it to the funciton, accept user input, and check

--- hangman.c
+++ hangman.c
@@ -4,24 +4,28 @@
 
 
 void hangy_hangy(char *string, char *hangman, char *current);
-int main(int argc, char* argv[])
+int main()
 {
-
   //TODO: turn string into a random word from a file
-  char string[32] = {'w','o','r','d'}; // initiate variable for testing
+  char strings[32]; // initiate variable for testing
+  char guess[32];
+  FILE *words;
+  words = fopen("words", "r");
+  fgets(guess, sizeof(guess), words);
+  strncpy(strings, guess, sizeof(strings));
+  char *string = strtok(strings, "\n ");
 
   //TODO: make this accomodate any sized word that comes in from the file.
   char hangman[32] = {'_','_','_','_'};
   
-  //this can stay the same
-  char guess[32];
   char fool[32];
 
   printf("%s:", hangman);
   
   //TODO: put the fgets in a loop to repeatedly ask for user input 
-  fgets(guess, 32, stdin);//takes in 32 bytes of user input, puts it in guess 
-  strncpy(fool, guess, 32);//copies whats in guess and puts it in fool 
+
+  fgets(guess, sizeof(guess), stdin);//takes in 32 bytes of user input, puts it in guess 
+  strncpy(fool, guess, sizeof(fool));//copies whats in guess and puts it in fool 
   char *current = strtok(fool, "\n ");// creates a token from the strtok
   printf("%zd\n", strlen(current));
   hangy_hangy(string, hangman, current);
