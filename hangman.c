2ad0c48a96e35f49e8fd8b2d5e8c41f39e8b2e71
Daniel Ems <danielems3@outlook.com>
2016-11-04 12:53:40 -0400

    Place stdin fgets in a while loop to allow users multiple guesses with feedback

--- hangman.c
+++ hangman.c
@@ -22,52 +22,63 @@ int main()
 
     //This while loop was found from Liam, and the if((rand() / (float)Rand_MAX
     //< (1.0 /++ a)
-    
     int num_lines= 0;
-    while(fgets(tmp_buf, sizeof(tmp_buf), words)){
-        if(is_valid(tmp_buf)){
-            if((rand() / (float)RAND_MAX) < (1.0 / ++num_lines)){
+    while(fgets(tmp_buf, sizeof(tmp_buf), words))
+    {
+        if(is_valid(tmp_buf))
+        {
+            if((rand() / (float)RAND_MAX) < (1.0 / ++num_lines))
+            {
                 strncpy(rand_word, tmp_buf, sizeof(rand_word));
             }
          }
     }
     
-    char *string = strtok(rand_word, "\n ");
+    strtok(rand_word, "\n ");
     //DEBUGGING PRINT STATEMENTS
     printf("number of good lines in a file: %d\n", num_lines);//debugging
     printf("whats is random word: %s\n", rand_word);//debugging
     printf("strlen(rand_word)%zd\n", strlen(rand_word));//debugging
-    printf("sizeof(rand_word)%zd\n", sizeof(rand_word));//debugging
 
     int word_length = strlen(rand_word) + 1;
     char *hangman = malloc(word_length);
-    strncpy(hangman, rand_word, word_length);
+
+    strncpy(hangman, rand_word, word_length);//copy my randomword to hangman
     make_hangman(hangman);
-  
-    char fool[32];
 
-    printf("%s:", hangman);
-  
-  //TODO: put the fgets in a loop to repeatedly ask for user input 
+    printf("%s:\n", hangman);
 
-  
-    fgets(tmp_buf, sizeof(tmp_buf), stdin);
-    strncpy(fool, tmp_buf, sizeof(fool));//copies whats in tmp_buf and puts it in fool 
-    char *current = strtok(fool, "\n ");// creates a token from the strtok
-    printf("%zd\n", strlen(current));
-    hangy_hangy(string, hangman, current);
+    //TODO: put the fgets in a loop to repeatedly ask for user input 
+while(1)
+ {
+    printf("hangman %s-, rand_word %s-\n", hangman, rand_word); 
+    strtok(tmp_buf, "\n ");// creates a token from the strtok
+    if(*hangman != *rand_word)
+    {
+       printf("%s:\n", hangman);
+       fgets(tmp_buf, sizeof(tmp_buf), stdin);
+       printf("tmp_buf%s-\n", tmp_buf);
+       hangy_hangy(rand_word, hangman, tmp_buf);
+     }
+     else
+     {
+      break;
+     }
+}
+
     fclose(words);
-
     free(hangman);
 }
   //a starting loop that checks and prints input, against words, and prints out
-void hangy_hangy(char *string, char *hangman, char *current)
+void hangy_hangy(char *rand_word, char *hangman, char *tmp_buf)
 {
-
-  for(int b=0; b<5; b++){
-    for(int a=0; a < 5; ++a){
-      if(current[b] == string[a]){
-        hangman[a] = current[b];
+  for(unsigned int b=0; b < strlen(rand_word); b++)
+  {
+    for(unsigned int a=0; a < strlen(rand_word); ++a)
+    {
+      if(tmp_buf[b] == rand_word[a])
+      {
+        hangman[a] = tmp_buf[b];
       }
     }
   }
@@ -80,10 +91,14 @@ bool is_valid(char *tmp_buf)
 {
     strtok(tmp_buf, "\n ");
     int a = 0;
-    while(tmp_buf[a]){
-      if(isalpha(tmp_buf[a])){
+    while(tmp_buf[a])
+    {
+      if(isalpha(tmp_buf[a]))
+      {
           a++;
-      }else{
+      }
+      else
+      {
           return false;
       }
     }
